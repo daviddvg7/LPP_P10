73f74af181884a881a469d5bc5ce2cf4cb40e38a
David Valverde <alu0101100296@ull.edu.es>
2019-11-12 20:15:39 +0000

    Código para obtener el alimento formateado

--- lib/comida/clase_comida.rb
+++ lib/comida/clase_comida.rb
@@ -1,9 +1,16 @@
 class Comida
-	attr_reader :nombre :gases :terreno
+	attr_reader :nombre :proteinas :carbos :lipidos :gases :terreno
 
-	def initialize(x, y, z)
-		@nombre=x
-		@gases=y
-		@terreno=z
+	def initialize(a, b, c, d, e, f)
+		@nombre=a
+		@proteinas=b
+		@carbos=c
+		@lipidos=d
+		@gases=e
+		@terreno=f
+	end
+
+	def to_s
+		puts "El alimento #{@nombre} posee #{@proteinas} g de proteínas, #{@carbos} g de carbohidratos, #{@lipidos} g de lípidos, genera #{@gases} kg de CO2 por porción y supone un uso de #{@terreno} m2 de terreno al año"
 	end
 end
