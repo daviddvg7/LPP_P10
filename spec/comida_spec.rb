6c9dec258d578ef353e5b5a9075f2f18c8a35675
David Valverde <alu0101100296@ull.edu.es>
2019-11-12 19:50:25 +0000

    Prueba de si existe la cantidad de terreno utilizado

--- spec/comida_spec.rb
+++ spec/comida_spec.rb
@@ -2,7 +2,7 @@ require 'lib/comida/clase_comida'
 
 RSpec.describe Comida do
   before (:all) do
-  	x=Comida.new("carne de vaca", 50.0)
+  	x=Comida.new("carne de vaca", 50.0, 164.0)
   end
 
   it "Debe existir un nombre para el alimento" do
@@ -11,6 +11,10 @@ RSpec.describe Comida do
 
   it "Debe existir la candidad de emisión de gases de efecto invernadero en kgCO2eq." do
 	expect(x.gases).not_to be nil
+  end
+
+  it "Debe existir la candidad de terreno utilizado en m2 año." do
+	expect(x.terreno).not_to be nil
   end
 
 end
