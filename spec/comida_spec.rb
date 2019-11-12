f7bcd16ade640ed85c7b375ce03e2062a70636d9
David Valverde <alu0101100296@ull.edu.es>
2019-11-12 19:47:49 +0000

    Prueba de si existe la cantidad de emisión de gases

--- spec/comida_spec.rb
+++ spec/comida_spec.rb
@@ -2,10 +2,15 @@ require 'lib/comida/clase_comida'
 
 RSpec.describe Comida do
   before (:all) do
-  	x=Comida.new("carne de vaca")
+  	x=Comida.new("carne de vaca", 50.0)
   end
 
   it "Debe existir un nombre para el alimento" do
-    expect(x.nombre).not_to be nil
+    	expect(x.nombre).not_to be nil
   end
+
+  it "Debe existir la candidad de emisión de gases de efecto invernadero en kgCO2eq." do
+	expect(x.gases).not_to be nil
+  end
+
 end
