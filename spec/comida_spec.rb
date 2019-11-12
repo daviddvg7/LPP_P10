261089db278537712ec7ac62c61117c5afa25f7e
David Valverde <alu0101100296@ull.edu.es>
2019-11-12 19:57:30 +0000

    Pruebas de métodos para obtener gases y terreno

--- spec/comida_spec.rb
+++ spec/comida_spec.rb
@@ -2,7 +2,7 @@ require 'lib/comida/clase_comida'
 
 RSpec.describe Comida do
   before (:all) do
-  	x=Comida.new("carne de vaca", 50.0, 164.0)
+  	x=Comida::Comida.new("carne de vaca", 50.0, 164.0)
   end
 
   it "Debe existir un nombre para el alimento" do
@@ -19,6 +19,14 @@ RSpec.describe Comida do
 	
   it "Existe un método para obtener el nombre del alimento." do
 	expect(x.nombre).to eq("carne de vaca")
+  end
+
+  it "Existe un método para obtener las emisiones de gases de efecto invernadero." do
+	expect(x.gases).to eq(50.0)
+  end
+
+  it "Existe un método para obtener el terreno utilizado." do
+	expect(x.terreno).to eq(164.0)
   end
 
 end
