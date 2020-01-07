68998864e1faaef8e9a1fe68ebd8e3f37af0a376
David Valverde <alu0101100296@ull.edu.es>
2020-01-07 11:43:17 +0000

    Menu DSL

--- lib/comida/menu.rb
+++ lib/comida/menu.rb
@@ -1,5 +1,50 @@
 class Menu
 
-    
+    attr_accessor :nombre,:descripcion, :componentes, :precio, :valor_amb, :valor_nutr
+    def initialize(nombre, &block)
+        @nombre= nombre
+        @componentes=[]
+
+        if block_given?
+            if block.arity == 1
+                yield self
+            else 
+                instance_eval(&block)
+            end
+        end
+    end
+
+    def descripcion(x)
+        @descripcion=x
+    end
+
+    def componente(nombre, options = {})
+       ingrediente=nombre
+       ingrediente << "(#{options[:precio]})" if options[:during]
+       
+       @componentes << ingrediente
+    end
+
+    def precio (x)
+        @precio=x
+    end
+
+    def valor_nutr (x)
+        @valor_nutr=x
+    end
+
+    def valor_amb(x)
+        @valor_amb=x
+    end
+
+    def to_s
+        output = "El menú #{@nombre} está compuesto por:\n"
+        @componentes.each_with_index do |componente, index|
+            output << " #{index + 1}) #{componente[:descripcion]} --> #{componente[:precio]} \n"
+        end
+
+        output << "Precio: #{@precio}\nValor nutricional: #{@valor_nutr} kcal\nValor ambiental: #{@valor_amb}"
+        output
+    end
 
 end
